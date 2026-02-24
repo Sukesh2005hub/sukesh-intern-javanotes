45689274232c73165eabf6a7f5f18cbfb05f589b
Sukeshkrishna E <sukeshkrishnae2005@gmail.com>
2026-02-24 22:31:28 +0530

    Update values of x and b in identifiers.java

--- identifiers.java
+++ identifiers.java
@@ -1,9 +1,9 @@
 public class identifiers {
 
-    int x=80;
+    int x=90;
   public static void main(String[] args) {
     int a=90;
-    int b=60;
+    int b=50;
     System.out.println(a);
      System.out.println(a);
   }
