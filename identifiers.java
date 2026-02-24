4d006db9651bdfb883cd9fc5b4632d6ed59b6d55
Sukeshkrishna E <sukeshkrishnae2005@gmail.com>
2026-02-24 22:55:54 +0530

    Change variable 'a' from 90 to 40

--- identifiers.java
+++ identifiers.java
@@ -2,7 +2,7 @@ public class identifiers {
 
     int x=100;
   public static void main(String[] args) {
-    int a=90;
+    int a=40;
     int b=50;
     System.out.println(a);
      System.out.println(b);
